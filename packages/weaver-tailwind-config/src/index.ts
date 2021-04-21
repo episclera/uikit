9bc7757f6598b6fc2d0d7742ec01bd44f678b468
Gherciu Gheorghe <gherciu553@gmail.com>
2021-04-21 18:46:46 +0300

    feat: add common vars to tw conf

--- packages/weaver-tailwind-config/src/index.ts
+++ packages/weaver-tailwind-config/src/index.ts
@@ -14,21 +14,44 @@ const weaverTailwindConfig: WeaverTailwindConfig = {
   theme: {
     extend: {
       colors: {
-        // NOTE: colors should be same as in ant-design default theme https://github.com/ant-design/ant-design/blob/0e4360038e79c7bd8bc7eb33dc56b7d0ccd481c4/components/style/themes/default.less
-        'primary-color': blue.primary,
-        'success-color': green.primary,
-        'warning-color': gold.primary,
-        'error-color': red[4],
-        color: 'rgba(0, 0, 0, 0.85)', // base text color
-        'color-secondary': 'rgba(0, 0, 0, 0.45)', // secondary text color
-        'disabled-color': 'rgba(0, 0, 0, 0.25)', // disable state color
+        // NOTE: vars should be same as in weaver/styles/vars.less or if not present there, then check ant-design default theme https://github.com/ant-design/ant-design/blob/0e4360038e79c7bd8bc7eb33dc56b7d0ccd481c4/components/style/themes/default.less
+        // -------- Colors -----------
+        'primary-color': blue[6],
+        'info-color': blue[6], // @primary-color
+        'success-color': green[6],
+        'processing-color': blue[6],
+        'error-color': red[5],
+        'warning-color': gold[6],
+        'normal-color': '#d9d9d9',
+        // -------- Link Colors -----------
+        'link-color': blue[6], // @primary-color
+        // -------- Items state Colors -----------
+        'item-active': 'rgb(230, 247, 255)', // @primary-1
+        'item-hover-bg': '#f5f5f5',
+        // -------- Typography -----------
+        'text-color': 'rgba(0, 0, 0, 0.85)', // fade(#000, 85%)
+        'text-color-secondary': 'rgba(0, 0, 0, 0.45)', // fade(#000, 45%)
+        'heading-color': 'rgba(0, 0, 0, 0.85)', // fade(#000, 85%)
+        // -------- Disabled states -----------
+        'disabled-color': 'rgba(0, 0, 0, 0.25)', // fade(#000, 25%)
+        'disabled-bg': 'rgb(245, 245, 245)', // hsv(0, 0, 96%)
+        // -------- Body colors -----------
+        'body-background': '#ffffff',
+        // -------- Base background color for most components -----------
+        'component-background': '#ffffff',
+        //  -------- Shadow colors ----------
+        'shadow-color': 'rgba(0, 0, 0, 0.15)',
+        // -------- Border colors -----------
+        'border-color-base': 'rgb(241, 241, 241)', // hsv(0, 0, 85%)
+        //  -------- Outline colors ----------
+        'outline-color': blue[6], // @primary-color
       },
     },
     container: {
       center: true, // To center containers
     },
     screens: {
-      // NOTE: breakpoints should be same as in ant-design default theme https://github.com/ant-design/ant-design/blob/0e4360038e79c7bd8bc7eb33dc56b7d0ccd481c4/components/style/themes/default.less
+      // NOTE: vars should be same as in weaver/styles/vars.less or if not present there, then check ant-design default theme https://github.com/ant-design/ant-design/blob/0e4360038e79c7bd8bc7eb33dc56b7d0ccd481c4/components/style/themes/default.less
       xs: { max: '576px' }, // < 576
       sm: '576px', // >= 576
       md: '768px', // >=768
