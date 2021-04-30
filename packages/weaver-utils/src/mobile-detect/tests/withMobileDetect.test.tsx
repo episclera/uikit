c4e96f05d96bf91ceb34108aafb77a08071b7487
Gherciu Gheorghe <gherciu553@gmail.com>
2021-04-30 20:28:48 +0300

    test: refactor async tests

--- packages/weaver-utils/src/mobile-detect/tests/withMobileDetect.test.tsx
+++ packages/weaver-utils/src/mobile-detect/tests/withMobileDetect.test.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-floating-promises, @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/ban-ts-comment */
 import React from 'react'
 import { NextComponent } from '../../../types'
 import withMobileDetect from '../withMobileDetect'
@@ -17,9 +17,7 @@ TestComponent.getInitialProps = () =>
   new Promise(resolve => resolve({ testProp: true }))
 
 describe('withMobileDetect', () => {
-  it('should detect correct device from window navigator user-agent if no request provided', () => {
-    expect.assertions(3)
-
+  it('should detect correct device from window navigator user-agent if no request provided', async () => {
     const withMobileDetectComponent = withMobileDetect(TestComponent)
 
     Object.defineProperty(global.window.navigator, 'userAgent', {
@@ -27,89 +25,75 @@ describe('withMobileDetect', () => {
       writable: true,
     })
 
-    withMobileDetectComponent.getInitialProps({}).then(props => {
-      expect(props.deviceContext.isMobile).toBeTruthy()
-    })
+    const checkMobileRes = await withMobileDetectComponent.getInitialProps({})
+    expect(checkMobileRes.deviceContext.isMobile).toBeTruthy()
 
     Object.defineProperty(global.window.navigator, 'userAgent', {
       value: userAgents.chromeTablet,
       writable: true,
     })
 
-    withMobileDetectComponent.getInitialProps({}).then(props => {
-      expect(props.deviceContext.isTablet).toBeTruthy()
-    })
+    const checkTabletRes = await withMobileDetectComponent.getInitialProps({})
+    expect(checkTabletRes.deviceContext.isTablet).toBeTruthy()
 
     Object.defineProperty(global.window.navigator, 'userAgent', {
       value: userAgents.chromeDesktop,
       writable: true,
     })
 
-    withMobileDetectComponent.getInitialProps({}).then(props => {
-      expect(props.deviceContext.isDesktop).toBeTruthy()
-    })
+    const checkDesktopRes = await withMobileDetectComponent.getInitialProps({})
+    expect(checkDesktopRes.deviceContext.isDesktop).toBeTruthy()
   })
 
-  it('should detect correct device from provided (different request format) user agent header', () => {
-    expect.assertions(3)
-
+  it('should detect correct device from provided (different request format) user agent header', async () => {
     const withMobileDetectComponent = withMobileDetect(TestComponent)
 
-    withMobileDetectComponent
-      .getInitialProps({
-        ctx: {
-          req: {
-            headers: {
-              'user-agent': userAgents.chromeMobile,
-            },
-          },
-        },
-      })
-      .then(props => {
-        expect(props.deviceContext.isMobile).toBeTruthy()
-      })
-
-    withMobileDetectComponent
-      .getInitialProps({
-        req: {
-          headers: {
-            'user-agent': userAgents.chromeTablet,
-          },
-        },
-      })
-      .then(props => {
-        expect(props.deviceContext.isTablet).toBeTruthy()
-      })
-
-    withMobileDetectComponent
-      .getInitialProps({
-        req: {
-          headers: {
-            'user-agent': userAgents.chromeDesktop,
-          },
-        },
-      })
-      .then(props => {
-        expect(props.deviceContext.isDesktop).toBeTruthy()
-      })
-  })
-
-  it('should correct re-implement getInitialProps method and merge component props with device detect props', () => {
-    expect.assertions(2)
-
-    const withMobileDetectComponent = withMobileDetect(TestComponent)
-
-    withMobileDetectComponent
-      .getInitialProps({
+    const checkMobileRes = await withMobileDetectComponent.getInitialProps({
+      ctx: {
         req: {
           headers: {
             'user-agent': userAgents.chromeMobile,
           },
         },
-      })
-      .then(props => {
-        expect(props).toHaveProperty('deviceContext')
-        expect(props).toHaveProperty('testProp')
-      })
+      },
+    })
+    expect(checkMobileRes.deviceContext.isMobile).toBeTruthy()
+
+    const checkTabletRes = await withMobileDetectComponent.getInitialProps({
+      ctx: {
+        req: {
+          headers: {
+            'user-agent': userAgents.chromeTablet,
+          },
+        },
+      },
+    })
+    expect(checkTabletRes.deviceContext.isTablet).toBeTruthy()
+
+    const checkDesktopRes = await withMobileDetectComponent.getInitialProps({
+      ctx: {
+        req: {
+          headers: {
+            'user-agent': userAgents.chromeDesktop,
+          },
+        },
+      },
+    })
+    expect(checkDesktopRes.deviceContext.isDesktop).toBeTruthy()
+  })
+
+  it('should correct re-implement getInitialProps method and merge component props with device detect props', async () => {
+    const withMobileDetectComponent = withMobileDetect(TestComponent)
+
+    const props = await withMobileDetectComponent.getInitialProps({
+      req: {
+        headers: {
+          'user-agent': userAgents.chromeMobile,
+        },
+      },
+    })
+
+    expect(props).toHaveProperty('deviceContext')
+    expect(props).toHaveProperty('testProp')
   })
 })
